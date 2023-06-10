45861eab58c994a4a7a17e843a44f96fa2a05bfd
Sophearith Saing <sophearithsaing123@gmail.com>
2023-06-11 03:37:08 +0700

    Add tabs

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 $(() => {
-  console.log('Hello World!');
-})
+  let data = $('.tab-item.selected').data('type');
+
+  $('.tab-item').click(function () {
+    $('.tab-item').removeClass('selected');
+    const selectedTabWidth = $(this).outerWidth();
+    const distance = $(this).position().left;
+    $('.tabs .indicator').css('transform', `translateX(${distance}px)`);
+    $(this).addClass('selected');
+
+    data = $(this).data('type');
+  });
+});

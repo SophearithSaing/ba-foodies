568ae214507f148202076ec01952232a64aac4a0
Sophearith Saing <sophearithsaing123@gmail.com>
2023-06-11 04:38:42 +0700

    Add output and functionality for equal

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(() => {
   let data = $('.tab-item.selected').data('type');
 
-  $('.tab-item').click(function () {
+  $('.tab-item').on('click', function () {
     $('.tab-item').removeClass('selected');
     const selectedTabWidth = $(this).outerWidth();
     const distance = $(this).position().left;
@@ -9,5 +9,14 @@ $(() => {
     $(this).addClass('selected');
 
     data = $(this).data('type');
+  });
+
+  $('.equal .calc').on('click', function () {
+    const price = $('.equal #price').val();
+    const people = $('.equal #people').val();
+    const amount = price / people;
+    const html = `<p>Each person has to pay ${amount} baht</p>`;
+    $('.output').append(html);
+    $('.output').slideDown();
   });
 });

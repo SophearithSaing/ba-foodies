becb30cd76c34bebaf7b05905f9efcc4029e1cad
Sophearith Saing <sophearithsaing123@gmail.com>
2023-06-11 22:09:22 +0700

    Add exclusion and addition ui functionality

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 $(() => {
   const output = $('.output');
   const outputContent = $('.output .content');
+
   let data = $('.tab-item.selected').data('type');
   $(`.${data}`).fadeIn({ duration: 200 });
+
+  bindAdditionAddFoodEvent();
+  bindExclusionAddFoodEvent();
 
   $('.tab-item').on('click', function () {
     $('.tab-item').removeClass('selected');
@@ -88,8 +92,124 @@ $(() => {
     output.slideDown();
   });
 
+  $('.other .shared .add').on('click', function () {
+    const rowCount = $('.other .shared-items .row.food').length;
+    const rowHtml = `
+      <div class="row food">
+        <input id="food-price-${
+          rowCount + 1
+        }" type="text" placeholder="Food Price ${rowCount + 1}" />
+      </div>
+    `;
+    $(rowHtml).insertAfter($('.other .shared-items .row.food')[rowCount - 1]);
+  });
+
+  $('.other .exclusion .add-exclusion-item').on('click', function () {
+    const rowCount = $('.other .exclusion-item').length;
+    const nextNumber = rowCount + 1;
+    const rowHtml = `
+      <div class="exclusion-item item-${nextNumber}">
+        <div class="row">
+          <input id="exclusion-${nextNumber}-name" type="text" placeholder="Name ${nextNumber}" />
+          <button class="add-food" data-number="${nextNumber}"><i class="bi bi-plus"></i></button>
+        </div>
+        <div class="row food">
+          <input
+            id="exclusion-${nextNumber}-price-1"
+            type="text"
+            placeholder="Food Price 1"
+          />
+          <button class="remove-food"><i class="bi bi-dash"></i></button>
+        </div>
+      </div>
+    `;
+    $(rowHtml).insertAfter($('.other .exclusion-item')[rowCount - 1]);
+
+    unbindExclusionAddFoodEvent();
+    bindExclusionAddFoodEvent();
+  });
+
+  $('.other .addition .add-addition-item').on('click', function () {
+    const rowCount = $('.other .addition-item').length;
+    const nextNumber = rowCount + 1;
+    const rowHtml = `
+      <div class="addition-item item-${nextNumber}">
+        <div class="row">
+          <input id="addition-${nextNumber}-name" type="text" placeholder="Name ${nextNumber}" />
+          <button class="add-food" data-number="${nextNumber}"><i class="bi bi-plus"></i></button>
+        </div>
+        <div class="row food">
+          <input
+            id="addition-${nextNumber}-price-1"
+            type="text"
+            placeholder="Food Price 1"
+          />
+          <button class="remove-food"><i class="bi bi-dash"></i></button>
+        </div>
+      </div>
+    `;
+    $(rowHtml).insertAfter($('.other .addition-item')[rowCount - 1]);
+
+    unbindAdditionAddFoodEvent();
+    bindAdditionAddFoodEvent();
+  });
+
   function resetOutput() {
     outputContent.empty();
     output.hide();
+  }
+
+  function bindExclusionAddFoodEvent() {
+    $('.other .exclusion-item .add-food').on('click', function () {
+      console.log('adding');
+      const itemNumber = $(this).data('number');
+      const rowCount = $(
+        `.other .exclusion-item.item-${itemNumber} .row.food`
+      ).length;
+      const nextNumber = rowCount + 1;
+      const rowHtml = `
+        <div class="row food">
+          <input
+            id="exclusion-${itemNumber}-price-${nextNumber}"
+            type="text"
+            placeholder="Food Price ${nextNumber}"
+          />
+        </div>
+      `;
+      $(rowHtml).insertAfter(
+        $(`.other .exclusion-item.item-${itemNumber} .row.food`)[rowCount - 1]
+      );
+    });
+  }
+
+  function unbindExclusionAddFoodEvent() {
+    $('.other .exclusion-item .add-food').off();
+  }
+
+  function bindAdditionAddFoodEvent() {
+    $('.other .addition-item .add-food').on('click', function () {
+      console.log('adding');
+      const itemNumber = $(this).data('number');
+      const rowCount = $(
+        `.other .addition-item.item-${itemNumber} .row.food`
+      ).length;
+      const nextNumber = rowCount + 1;
+      const rowHtml = `
+        <div class="row food">
+          <input
+            id="addition-${itemNumber}-price-${nextNumber}"
+            type="text"
+            placeholder="Food Price ${nextNumber}"
+          />
+        </div>
+      `;
+      $(rowHtml).insertAfter(
+        $(`.other .addition-item.item-${itemNumber} .row.food`)[rowCount - 1]
+      );
+    });
+  }
+
+  function unbindAdditionAddFoodEvent() {
+    $('.other .addition-item .add-food').off();
   }
 });
